cc08dcefdfbf59307b9d1819e92b6d138d6e0b25
Henrique Goulart <henriquedsg89@gmail.com>
2018-02-19 09:24:25 -0300

    Update zeppelin to latest version and spark memory parameter

--- remote_hook.sh
+++ remote_hook.sh
@@ -59,7 +59,7 @@ on_trap_exit() {
 
 install_and_run_zeppelin() {
     if [[ ! -d "zeppelin" ]]; then
-        wget "http://www.us.apache.org/dist/incubator/zeppelin/0.5.6-incubating/zeppelin-0.5.6-incubating-bin-all.tgz" -O zeppelin.tar.gz
+        wget "http://www-us.apache.org/dist/zeppelin/zeppelin-0.7.3/zeppelin-0.7.3-bin-all.tgz" -O zeppelin.tar.gz
         mkdir zeppelin
         tar xvzf zeppelin.tar.gz -C zeppelin --strip-components 1 > /tmp/zeppelin_install.log
     fi
@@ -67,7 +67,7 @@ install_and_run_zeppelin() {
         export MASTER="${JOB_MASTER}"
         export ZEPPELIN_PORT="8081"
         export SPARK_HOME="/root/spark"
-        export SPARK_SUBMIT_OPTIONS="--jars ${JAR_PATH} --runner-executor-memory ${SPARK_MEM_PARAM}"
+        export SPARK_SUBMIT_OPTIONS="--jars ${JAR_PATH} --executor-memory ${SPARK_MEM_PARAM}"
         sudo -E zeppelin/bin/zeppelin.sh
     else
         notify_error_and_exit "Zepellin installation not found"
